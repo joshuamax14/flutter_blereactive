3eb39e4cc6d5fe4d9284591438f4e4cfcc394d58
joshuamax14 <rafaelvillavicencio14@gmail.com>
2024-06-06 23:10:49 +0800

    added containers to make sure some gif and the graph will not overflow

--- new_project/lib/Screens/Intropage.dart
+++ new_project/lib/Screens/Intropage.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:new_project/Providers/UsernameProvider.dart';
 import 'package:new_project/Screens/ChangeUser.dart';
 import 'package:new_project/Screens/Homepage2.dart';
-//import 'package:new_project/Screens/Homepage.dart';
 import 'package:provider/provider.dart';
 
 class Intropage extends StatelessWidget {
-  const Intropage({super.key});
+  const Intropage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,40 +19,51 @@ class Intropage extends StatelessWidget {
           )
         ],
       ),
-      backgroundColor: Colors.white, // Set the background color to white
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              context.watch<Usernameprovider>().username,
+              'Welcome ${context.watch<Usernameprovider>().username}!',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 25,
               ),
             ),
 
-            SizedBox(height: 20), // Add some space between the text and the image
-            Image.asset('lib/Screens/assets/walking.gif'), // Placeholder for your moving picture
-            SizedBox(height: 20), // Add some space between the image and the buttons
+            SizedBox(height: 20),
+            // Wrap the image with a Container to adjust its size
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8, // 80% of the screen width
+              height: MediaQuery.of(context).size.height * 0.3, // 30% of the screen height
+              child: Image.asset('lib/Screens/assets/walking.gif'),
+            ),
+
+            SizedBox(height: 20),
             ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Changeuser(),
-                      ));
-                },
-                child: Text('Change Patient Information')),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Changeuser(),
+                  ),
+                );
+              },
+              child: Text('Change Patient Information'),
+            ),
+             SizedBox(height: 10),
             ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Homepage2(),
-                      ));
-                },
-                child: Text('Home'))
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Homepage2(),
+                  ),
+                );
+              },
+              child: Text('Home'),
+            ),
           ],
         ),
       ),
